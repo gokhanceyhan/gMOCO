7e08c5fb9cd6b8e00e9e1709fa3efee8fb9c8b47
gokhanceyhan <gokhanceyhan01@gmail.com>
2017-10-09 14:42:54 +0300

    first version completed. needs feedback

--- gMOCO/app.R
+++ gMOCO/app.R
@@ -1,11 +1,13 @@
 #install.packages("shiny")
 #install.packages("shinydashboard")
 #install.packages("plotly")
+#install.packages("DT")
 
 
 library(shiny)
 library(shinydashboard)
 library(plotly)
+library(DT)
 
 
 
@@ -28,11 +30,6 @@ ui <- dashboardPage(
     tabItems(
       tabItem(tabName = "dashboard",
               fluidRow(
-                valueBoxOutput("numOfObjectives"),
-                valueBoxOutput("numOfPoints"),
-                valueBoxOutput("coverage")
-              ),
-              fluidRow(
                 tabBox(
                   title = "Inputs", width = 4,
                   # The id lets us use input$tabset1 on the server to find the current tab
@@ -43,17 +40,19 @@ ui <- dashboardPage(
                   ),
                   tabPanel("z2",
                            "Select a range for criterion values:",
-                           sliderInput("slider2", "z1:", 1, 100, value = c(1,100), dragRange = TRUE)
+                           sliderInput("slider2", "z2:", 1, 100, value = c(1,100), dragRange = TRUE)
                   ),
                   tabPanel("z3",
                            "Select a range for criterion values:",
-                           sliderInput("slider3", "z1:", 1, 100, value = c(1,100), dragRange = TRUE)
+                           sliderInput("slider3", "z3:", 1, 100, value = c(1,100), dragRange = TRUE)
                   )
-                )
+                ),
+                valueBoxOutput(width = 4, "numOfObjectives"),
+                valueBoxOutput(width = 4, "numOfPoints")
               ),
               fluidRow(
                 box(
-                  title = "Scatter Plot", status = "success", solidHeader = TRUE,
+                  title = "Scatter Plot: Filtered Points", status = "success", solidHeader = TRUE,
                   collapsible = TRUE,
                   selectInput("scatterPlotSelect1", "Select 2 or 3 criteria:",
                               c("z1", "z2", "z3"), selected = c("z1", "z2", "z3"), multiple = TRUE
@@ -61,14 +60,27 @@ ui <- dashboardPage(
                   plotlyOutput("plot1")
                 ),
                 box(
-                  title = "Parallel Coordinates Plot", status = "warning", solidHeader = TRUE,
+                  title = "Parallel Coordinates Plot: Filtered Points", status = "warning", solidHeader = TRUE,
                   collapsible = TRUE,
                   plotlyOutput("plot2")
+                )
+              ),
+              fluidRow(
+                box(title = "Filtered Points", width = 6, DT::dataTableOutput("tableFiltered")),
+                box(
+                  title = "Scatter Plot: Selected Points", width = 6, status = "success", solidHeader = TRUE,
+                  collapsible = TRUE,
+                  plotlyOutput("plot11")
+                ),
+                box(
+                  title = "Parallel Coordinates Plot: Selected Points", width = 6, status = "warning", solidHeader = TRUE,
+                  collapsible = TRUE,
+                  plotlyOutput("plot21")
                 )
               )
       ),
       tabItem(tabName = "data",
-              tableOutput("contents")
+              DT::dataTableOutput("table")
               
       )
     )
@@ -76,7 +88,7 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output, session) {
-  output$contents <- renderTable({
+  output$table <- DT::renderDataTable({
     inFile <- input$file1
     
     if (is.null(inFile))
@@ -89,13 +101,10 @@ server <- function(input, output, session) {
     npoints = nrow(ndpoints)
     
     output$numOfObjectives <- renderValueBox({
-      valueBox(nobj, "criteria", icon = icon("arrows"), color = "blue")
+      valueBox(nobj, "criteria", icon = icon("arrows"), color = "red")
     })
     output$numOfPoints <- renderValueBox({
-      valueBox(npoints, "nondominated points", icon = icon("archive"), color = "olive")
-    })
-    output$coverage <- renderValueBox({
-      valueBox(10, " % coverage gap", icon = icon("pie-chart"), color = "orange")
+      valueBox(npoints, "nondominated points", icon = icon("archive"), color = "blue")
     })
     
     # update the slider input box
@@ -115,8 +124,18 @@ server <- function(input, output, session) {
                       value = c(min,max)
     )
     
+    # render list of points
+    output$tableFiltered <- DT::renderDataTable({
+      filtered_points <- ndpoints[which(ndpoints$z1 >= input$slider1[1]
+                                        & ndpoints$z1 <= input$slider1[2]
+                                        & ndpoints$z2 >= input$slider2[1]
+                                        & ndpoints$z2 <= input$slider2[2]
+                                        & ndpoints$z3 >= input$slider3[1]
+                                        & ndpoints$z3 <= input$slider3[2]),]
+    })
     
-    # render scatter plot
+    
+    # render scatter plot1
     output$plot1 <- renderPlotly({
       filtered_points <- ndpoints[which(ndpoints$z1 >= input$slider1[1]
                                         & ndpoints$z1 <= input$slider1[2]
@@ -124,12 +143,14 @@ server <- function(input, output, session) {
                                         & ndpoints$z2 <= input$slider2[2]
                                         & ndpoints$z3 >= input$slider3[1]
                                         & ndpoints$z3 <= input$slider3[2]),]
+      s = input$tableFiltered_rows_selected
+      selected_points <- filtered_points[s,]
       if(NROW(input$scatterPlotSelect1) < 2 | NROW(input$scatterPlotSelect1) > 3  )
         return (NULL)
       else if(NROW(input$scatterPlotSelect1) ==2){
         if((input$scatterPlotSelect1[1]=="z1" & input$scatterPlotSelect1[2]=="z2")|
            (input$scatterPlotSelect1[1]=="z2" & input$scatterPlotSelect1[2]=="z1")){
-          p <- plot_ly(filtered_points, x = ~z1, y = ~z2) %>%
+          p <- plot_ly(filtered_points, x = ~z1, y = ~z2, color = ~z3) %>%
             add_markers() %>%
             layout(scene = list(xaxis = list(title = 'z1'),
                                 yaxis = list(title = 'z2')))
@@ -137,7 +158,7 @@ server <- function(input, output, session) {
         }
         if((input$scatterPlotSelect1[1]=="z1" & input$scatterPlotSelect1[2]=="z3")|
            (input$scatterPlotSelect1[1]=="z3" & input$scatterPlotSelect1[2]=="z1")){
-          p <- plot_ly(filtered_points, x = ~z1, y = ~z3) %>%
+          p <- plot_ly(filtered_points, x = ~z1, y = ~z3, color = ~z2) %>%
             add_markers() %>%
             layout(scene = list(xaxis = list(title = 'z1'),
                                 yaxis = list(title = 'z3')))
@@ -145,7 +166,7 @@ server <- function(input, output, session) {
         }
         if((input$scatterPlotSelect1[1]=="z2" & input$scatterPlotSelect1[2]=="z3")|
            (input$scatterPlotSelect1[1]=="z3" & input$scatterPlotSelect1[2]=="z2")){
-          p <- plot_ly(filtered_points, x = ~z2, y = ~z3) %>%
+          p <- plot_ly(filtered_points, x = ~z2, y = ~z3, color = ~z1) %>%
             add_markers() %>%
             layout(scene = list(xaxis = list(title = 'z2'),
                                 yaxis = list(title = 'z3')))
@@ -162,7 +183,55 @@ server <- function(input, output, session) {
       }
     })
     
-    # render parallel coordinates plot
+    # render scatter plot11
+    output$plot11 <- renderPlotly({
+      filtered_points <- ndpoints[which(ndpoints$z1 >= input$slider1[1]
+                                        & ndpoints$z1 <= input$slider1[2]
+                                        & ndpoints$z2 >= input$slider2[1]
+                                        & ndpoints$z2 <= input$slider2[2]
+                                        & ndpoints$z3 >= input$slider3[1]
+                                        & ndpoints$z3 <= input$slider3[2]),]
+      s = input$tableFiltered_rows_selected
+      selected_points <- filtered_points[s,]
+      if(NROW(input$scatterPlotSelect1) < 2 | NROW(input$scatterPlotSelect1) > 3  )
+        return (NULL)
+      else if(NROW(input$scatterPlotSelect1) ==2){
+        if((input$scatterPlotSelect1[1]=="z1" & input$scatterPlotSelect1[2]=="z2")|
+           (input$scatterPlotSelect1[1]=="z2" & input$scatterPlotSelect1[2]=="z1")){
+          p <- plot_ly(selected_points, x = ~z1, y = ~z2) %>%
+            add_markers() %>%
+            layout(scene = list(xaxis = list(title = 'z1'),
+                                yaxis = list(title = 'z2')))
+          return (p)
+        }
+        if((input$scatterPlotSelect1[1]=="z1" & input$scatterPlotSelect1[2]=="z3")|
+           (input$scatterPlotSelect1[1]=="z3" & input$scatterPlotSelect1[2]=="z1")){
+          p <- plot_ly(selected_points, x = ~z1, y = ~z3) %>%
+            add_markers() %>%
+            layout(scene = list(xaxis = list(title = 'z1'),
+                                yaxis = list(title = 'z3')))
+          return (p)
+        }
+        if((input$scatterPlotSelect1[1]=="z2" & input$scatterPlotSelect1[2]=="z3")|
+           (input$scatterPlotSelect1[1]=="z3" & input$scatterPlotSelect1[2]=="z2")){
+          p <- plot_ly(selected_points, x = ~z2, y = ~z3) %>%
+            add_markers() %>%
+            layout(scene = list(xaxis = list(title = 'z2'),
+                                yaxis = list(title = 'z3')))
+          return (p)
+        }
+      } 
+      else {
+        p <- plot_ly(selected_points, x = ~z1, y = ~z2, z = ~z3) %>%
+          add_markers() %>%
+          layout(scene = list(xaxis = list(title = 'z1'),
+                              yaxis = list(title = 'z2'),
+                              zaxis = list(title = 'z3')))
+        return (p)
+      }
+    })
+    
+    # render parallel coordinates plot2
     output$plot2 <- renderPlotly({
       filtered_points <- ndpoints[which(ndpoints$z1 >= input$slider1[1]
                                         & ndpoints$z1 <= input$slider1[2]
@@ -170,7 +239,9 @@ server <- function(input, output, session) {
                                         & ndpoints$z2 <= input$slider2[2]
                                         & ndpoints$z3 >= input$slider3[1]
                                         & ndpoints$z3 <= input$slider3[2]),]
-      p <- plot_ly(type = 'parcoords', line = list(color = 'blue'),
+      s = input$tableFiltered_rows_selected
+      selected_points <- filtered_points[s,]
+      p <- plot_ly(type = 'parcoords', line = list(color = 'light-blue'),
                    dimensions = list(
                      list(range = c(input$slider1[1],input$slider1[2]),
                           label = 'z1', values = filtered_points$z1),
@@ -183,6 +254,28 @@ server <- function(input, output, session) {
       return (p)
     })
     
+    # render parallel coordinates plot21
+    output$plot21 <- renderPlotly({
+      filtered_points <- ndpoints[which(ndpoints$z1 >= input$slider1[1]
+                                        & ndpoints$z1 <= input$slider1[2]
+                                        & ndpoints$z2 >= input$slider2[1]
+                                        & ndpoints$z2 <= input$slider2[2]
+                                        & ndpoints$z3 >= input$slider3[1]
+                                        & ndpoints$z3 <= input$slider3[2]),]
+      s = input$tableFiltered_rows_selected
+      selected_points <- filtered_points[s,]
+      p <- plot_ly(type = 'parcoords', line = list(color = 'navy'),
+                   dimensions = list(
+                     list(range = c(input$slider1[1],input$slider1[2]),
+                          label = 'z1', values = selected_points$z1),
+                     list(range = c(input$slider2[1],input$slider2[2]),
+                          label = 'z2', values = selected_points$z2),
+                     list(range = c(input$slider3[1],input$slider3[2]),
+                          label = 'z3', values = selected_points$z3)
+                   )
+      )
+      return (p)
+    })
     
     return (ndpoints)
   })
